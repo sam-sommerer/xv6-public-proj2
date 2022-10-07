2907fe239c385c50f106230b264c633f6b3834a6
sam-sommerer <sommerer@usc.edu>
2022-10-06 21:48:24 -0700

    fix deadlock

--- race.c
+++ race.c
@@ -3,9 +3,11 @@
 #include "user.h"
 // #include "defs.h"
 #include "condvar.h"
+#include "fcntl.h"
 //We want Child 1 to execute first, then Child 2, and finally Parent.
 int main() {
     struct condvar cv;
+    int fd = open("flag", O_RDWR | O_CREATE);
     init_lock(&cv.lk);
 
     int pid = fork(); //fork the first child
@@ -16,6 +18,7 @@ int main() {
         printf(1, "Child 1 Executing\n");
 
         lock(&cv.lk);
+        write(fd, "done", 4);
         cv_signal(&cv);
         unlock(&cv.lk);
     } else {
@@ -23,11 +26,17 @@ int main() {
         if(pid < 0) {
             printf(1, "Error forking second child.\n");
         } else if(pid == 0) {
-            printf(1, "Child 2 Executing\n");
-
             lock(&cv.lk);
-            cv_wait(&cv);
+            struct stat stats;
+            fstat(fd, &stats);
+            printf(1, "file size = %d\n", stats.size);
+            while(stats.size <= 0){
+                cv_wait(&cv);
+                fstat(fd, &stats);
+                printf(1, "file size = %d\n", stats.size);
+            }
             unlock(&cv.lk);
+            printf(1, "Child 2 Executing\n");
         } else {
             printf(1, "Parent Waiting\n");
             int i;
@@ -38,5 +47,7 @@ int main() {
             printf(1, "Parent exiting.\n");
         }
     }
+    close(fd);
+    unlink("flag");
     exit();
 }
