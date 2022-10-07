c47d1bf97b3c512ab7391a7fe3e052efbe7b6cf3
sam-sommerer <sommerer@usc.edu>
2022-10-06 18:52:56 -0700

    added condvar stuff

--- user.h
+++ user.h
@@ -1,5 +1,6 @@
 struct stat;
 struct rtcdate;
+struct condvar;
 
 // system calls
 int fork(void);
@@ -23,6 +24,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int cv_wait(struct condvar *);
+int cv_signal(struct condvar *);
 
 // ulib.c
 int stat(const char*, struct stat*);
